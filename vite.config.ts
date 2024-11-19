9aebe25495654be8ebf4632d1efd7e25fe7dc300
Ridwan Sharkar <ridwansharkar@gmail.com>
2024-11-18 23:53:18 -0500

    thomas u dork

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   base: '/Borrowed-Order/',
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: true
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './Borrowed-Order')
+    }
   }
 })
