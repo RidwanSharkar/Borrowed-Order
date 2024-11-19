8aa0e4f08dad4cd82dc9931618145534e22551d7
Ridwan Sharkar <ridwansharkar@gmail.com>
2024-11-18 23:47:25 -0500

    or am i making this up

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/Borrowed-Order/',
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true
+  }
 })
